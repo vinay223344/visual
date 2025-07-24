69fc47d61d93ed14d85fe2740183df7ea7cc3f39
vinay223344 <v647414@gmail.com>
2025-07-24 10:54:04 +0530

    Uploading Stock.cs via API

---
+++ Stock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace firstlab
+{
+    class Stock
+    {
+        string s_name, sym_name;
+        double prev_price, curr_price;
+        public Stock(string name, string sym_name, double prev_price, double curr_price)
+        {
+            s_name = name;
+            this.sym_name = sym_name;
+            this.prev_price = prev_price;
+            this.curr_price = curr_price;
+        }
+        public double getChangePercentage()
+        {
+            return (curr_price - prev_price) / prev_price * 100;
+        }
+
+    }
+}
