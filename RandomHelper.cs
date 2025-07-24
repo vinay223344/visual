d9dc9093bcac795f7fe6dbf1e95df44a2a365b30
vinay223344 <v647414@gmail.com>
2025-07-24 10:53:56 +0530

    Uploading RandomHelper.cs via API

---
+++ RandomHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace firstlab
+{
+    class RandomHelper
+    {
+        public static int randint(int a, int b)
+        {
+            Random rnd = new Random();
+            return rnd.Next(a, b + 1);
+        }
+        public static double randdouble(int  a, int b)
+        {
+            Random rnd = new Random();
+            return rnd.NextDouble()*(b-a)+a;
+        }
+
+    }
+}
